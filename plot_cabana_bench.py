56f6e0c454d9e605d55671f2bbf1b591891c0b55
Sam Reeve <6740307+streeve@users.noreply.github.com>
2022-02-11 23:35:31 -0500

    more MPI fixup

--- plot_cabana_bench.py
+++ plot_cabana_bench.py
@@ -11,19 +11,19 @@ plt.rcParams["font.size"] = 12
 
 all_backends = ['host', 'serial', 'openmp', 'cuda', 'hip', 'cudauvm']
 
-comm = True
-#backends = ['cuda', 'hip']
-backends = ['cuda_cuda', 'hip_hip']
+backends = ['cuda', 'hip']
+#backends = ['cuda_cuda', 'hip_hip']
 
 size_list = [1e3, 1e4, 1e5, 1e6, 1e7]
-type_list = ['create', 'migrate']
-#type_list = ['create', 'permute']
+#type_list = ['create', 'migrate']
+type_list = ['create', 'permute']
 #type_list = ['create', 'iteration']
 
 param_list = ['dist']
-#param_list = ['10', '100', '1000', '10000']
+#param_list = ['10', '100', '1000', '10000', '100000', '1000000', '10000000'] #['sort']
 #param_list = ['3', '4']
 
+comm = 'dist' in param_list or 'halo' in param_list
 
 color_dict = {type_list[0]: '#E31A1C', type_list[1]:'#4291C7'}
 width = 0.1
@@ -32,7 +32,8 @@ filenames = sys.argv[1:]
 n_files = len(filenames)
 n_sizes = len(size_list)
 
-backend_dict = {key: {key: {key: np.zeros([n_sizes]) for key in type_list} for key in param_list} for key in backends}
+backend_dict = {key: {key: {key: [] for key in type_list} for key in param_list} for key in backends}
+sizes_dict = {key: {key: {key: [] for key in type_list} for key in param_list} for key in backends}
 
 for file in filenames:
     if comm:
@@ -43,7 +44,6 @@ for file in filenames:
 
     l = 0
     while l < len(txt):
-        print(l)
         if txt[l].isspace() or 'problem_size' in txt[l]:
             l += 1
         elif "Cabana Comm" in txt[l]:
@@ -53,10 +53,9 @@ for file in filenames:
             for backend in backends:
                 if backend in txt[l]:
                     current_backend = backend
-            print(current_backend)
             current_param = None
             for param in param_list:
-                if param in txt[l]:
+                if param in txt[l] and "." not in txt[l]:
                     current_param = param
             for type in type_list:
                 if type in txt[l]:
@@ -76,10 +75,12 @@ for file in filenames:
                 read_size = float(vals[0])
             for s, size in enumerate(size_list):
                 if size == read_size:
-                    backend_dict[current_backend][current_param][current_type][s] = float(vals[-1])
+                    backend_dict[current_backend][current_param][current_type].append(float(vals[-1]))
+                    sizes_dict[current_backend][current_param][current_type].append(size)
             l += 1
 
 print(backend_dict)
+print(sizes_dict)
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
@@ -92,10 +93,11 @@ ax1 = fig.add_subplot(111)
 '''
 for param in param_list:
     for type in type_list:
-        y = backend_dict[backends[0]][param][type] / backend_dict[backends[1]][param][type]
-        plt.plot(size_list, y, width, color=color_dict[type], lw=2)
+        for size in size_list:
+            y = np.array(backend_dict[backends[0]][param][type]) / np.array(backend_dict[backends[1]][param][type])
+            plt.plot(sizes_dict[backends[0]][param][type], y, color=color_dict[type], lw=4)#, facecolors=color_dict[type])
 
-ax1.set_ylabel("Time (s)")
+ax1.set_ylabel("Speedup relative to V100-CUDA")
 
 fake_lines = [Line2D([0], [0], color=color_dict[type_list[0]], lw=2, label=type_list[0]),
               Line2D([0], [0], color=color_dict[type_list[1]], lw=2, label=type_list[1])]
